f1024eb770ca58c1148c6a3510876f18ec17d063
vrdiy <amverdigo@gmail.com>
2022-10-19 22:27:10 -0400

    Cart json in js

--- ticketapp/static/ticketapp/cart.js
+++ ticketapp/static/ticketapp/cart.js
@@ -5,18 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
     cartlist = document.querySelector("#cart");
     
     cartlist.innerHTML = ''
-    //console.log(tickets);
-    //tickets = JSON.stringify(tickets1.ticket)
-    //console.log(typeof tickets1.ticket);
-    //tickets = tickets.pop()
-    //tickets = tickets.shift()
-    //tickets = JSON.parse(tickets);
-    //console.log(tickets[0]);
-    /*
+    console.log(tickets);
+
+    
     for (let i = 0; i < tickets.length; i++) {
-        formattedTicket = tickets[i];
+        formattedTicket = JSON.parse(tickets[i]);
         li = document.createElement("li");
-        li.innerHTML = formattedTicket;
+        li.innerHTML = formattedTicket.showing.movie.film;
         cartlist.append(li);
-    };*/
+    };
 })
